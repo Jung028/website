83d55b84354932e862b49fa85df2d41aea7d56ac
Adam Lim <79502321+Jung028@users.noreply.github.com>
2023-07-07 09:54:15 +0800

    warn app.js

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+import Experience from './Pages/Experience';
+import Footer from './Components/Footer';
+
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+        <Footer />
+
+      </div>
+    </Router>
+    
+  );
+};
+
+export default App;
